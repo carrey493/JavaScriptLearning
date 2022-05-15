94bd2f4db58dd786d16a62fd64955d1e26afce90
Atrox493 <2577632081@qq.com>
2022-05-16 00:27:28 +0800

    构造函数测试和完善

--- Promise Learning/README.md
+++ Promise Learning/README.md
@@ -273,3 +273,61 @@ p.then(
 })(window)
 ```
 
+### 3.2构造函数实现与完善
+
+```js
+function Promise(excutor) {
+        //当前Promise对象保存起来
+        const self = this
+        self.status = 'pending' //给promise对象指定status属性，初始值为pending
+        self.data = undefined //给promise对象指定一个用于存储结果数据的属性
+        self.callbacks = [] //每个元素的结构：{onResolved(){},onRejected(){}}
+        function resolve(value) {
+            //如果当前状态不是pending,直接结束
+            if (self.status !== 'pending') {
+                return
+            }
+            //将状态改为resolved
+            self.status = 'resolved'
+            //保存value数据
+            self.data = value
+            //如果有待执行callback函数，立即异步执行回调函数onResolved
+            if (self.callbacks.length > 0) {
+                setTimeout(() => { //放入队列中执行所有成功的回调
+                    self.callbacks.forEach(callbacksObj => {
+                        callbacksObj.onResolved(value)
+                    })
+                })
+            }
+        }
+
+        function reject(reason) {
+            //如果当前状态不是pending,直接结束
+            if (self.status !== 'pending') {
+                return
+            }
+
+            //将状态改为rejected
+            self.status = 'rejected'
+            //保存value数据
+            self.data = reason
+            //如果有待执行callback函数，立即异步执行回调函数onResolved
+            if (self.callbacks.length > 0) {
+                setTimeout(() => { //放入队列中执行所有失败的回调
+                    self.callbacks.forEach(callbacksObj => {
+                        callbacksObj.onRejected(reason)
+                    })
+                })
+            }
+        }
+
+        //立即同步执行excutor
+        try {
+            excutor(resolve, reject)
+        } catch (error) { //如果执行器抛出异常,promise对象变为REJECTED状态
+            reject(error)
+        }
+
+    }
+```
+
--- Promise Learning/lib/Promise.js
+++ Promise Learning/lib/Promise.js
@@ -8,6 +8,56 @@
         excutor 执行器函数(同步执行)
     */
     function Promise(excutor) {
+        //当前Promise对象保存起来
+        const self = this
+        self.status = 'pending' //给promise对象指定status属性，初始值为pending
+        self.data = undefined //给promise对象指定一个用于存储结果数据的属性
+        self.callbacks = [] //每个元素的结构：{onResolved(){},onRejected(){}}
+        function resolve(value) {
+            //如果当前状态不是pending,直接结束
+            if (self.status !== 'pending') {
+                return
+            }
+            //将状态改为resolved
+            self.status = 'resolved'
+            //保存value数据
+            self.data = value
+            //如果有待执行callback函数，立即异步执行回调函数onResolved
+            if (self.callbacks.length > 0) {
+                setTimeout(() => { //放入队列中执行所有成功的回调
+                    self.callbacks.forEach(callbacksObj => {
+                        callbacksObj.onResolved(value)
+                    })
+                })
+            }
+        }
+
+        function reject(reason) {
+            //如果当前状态不是pending,直接结束
+            if (self.status !== 'pending') {
+                return
+            }
+
+            //将状态改为rejected
+            self.status = 'rejected'
+            //保存value数据
+            self.data = reason
+            //如果有待执行callback函数，立即异步执行回调函数onResolved
+            if (self.callbacks.length > 0) {
+                setTimeout(() => { //放入队列中执行所有失败的回调
+                    self.callbacks.forEach(callbacksObj => {
+                        callbacksObj.onRejected(reason)
+                    })
+                })
+            }
+        }
+
+        //立即同步执行excutor
+        try {
+            excutor(resolve, reject)
+        } catch (error) { //如果执行器抛出异常,promise对象变为REJECTED状态
+            reject(error)
+        }
 
     }
     /* 
@@ -15,8 +65,13 @@
     指定成功和函数的回调函数
     返回一个新的promise对象
     */
-    Promise.prototype.then = function (onResolved, onResolved) {
-
+    Promise.prototype.then = function (onResolved, onRejected) {
+        const self = this
+        //假设当前状态还是pending状态，将回调函数保存起来
+        self.callbacks.push({
+            onResolved,
+            onRejected
+        })
     }
     /* 
     Promise原型对象的catch()
