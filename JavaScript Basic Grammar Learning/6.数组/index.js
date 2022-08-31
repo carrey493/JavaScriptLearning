a8e6958a41633bd1466e491f1286824fb48643cd
Atrox493 <2577632081@qq.com>
2022-09-01 01:40:19 +0800

    获取数组元素

--- JavaScript Basic Grammar Learning/6.数组/index.js
+++ JavaScript Basic Grammar Learning/6.数组/index.js
@@ -3,7 +3,11 @@
 var arr = new Array();//创建了一个空的数组
 // 3．利用数组字面量创建数组[]
 var arr = [];//创建了一个空的数组
-var arr1 = [12, 'pink老师', true];
+var arr1 = [1, 2, '老师', true];
 //4．我们数组里面的数据一定用逗号分隔
 //5．数组里面的数据比如 1,2,true，我们称为数组元素
+//6. 获取数组元素 格式数组名[索引号]
+var arr1 = [1, 2, '老师', true];
+console.log(arr1[2]);//老师
+console.log(arr1[5]);//undefined
 
